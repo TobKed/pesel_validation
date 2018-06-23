503b197298173bf8022f179c68daea2b442e3a68
Tobiasz Kedzierski <tobiaszkedzierski@gmail.com>
2018-06-23 22:19:03 +0200

    release

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['post', 'get'])
+def index():
+    if request.method == 'POST':
+        pesel = request.form.get('pesel')
+        if pesel:
+            return render_template("index.html", pesel_valid=validate_pesel(pesel), pesel=pesel)
+    return render_template("index.html", pesel_valid=None, pesel=None)
+
+
+def validate_pesel(pesel):
+    weights = "13791379131"
+    if not isinstance(pesel, str):
+        pesel = str(pesel).zfill(11)
+    if not pesel.isdigit() or len(pesel) != 11:
+        return False
+    weighted_sum = 0
+    for x, y in zip(pesel, weights):
+        weighted_sum += int(x) * int(y)
+    return not weighted_sum % 10
+
+
+if __name__ == '__main__':
+    app.run()
